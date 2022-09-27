75fdb6e7fffee6af28d2690555f7e6fd67e24982
intersoom <sallybig@naver.com>
2022-09-27 15:00:49 +0900

    🐛 router 버그 픽스

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Login from './features/auth/pages/Login'
 import Header from './layout/header/Header'
 import Signup from './features/auth/pages/Signup'
@@ -12,12 +12,10 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   )
